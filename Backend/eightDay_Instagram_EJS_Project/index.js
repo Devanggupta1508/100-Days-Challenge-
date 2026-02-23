e37845b359884adaa61208cddc81f6e240f70b39
Devang Gupta <devanggupta1508@gmail.com>
2026-02-23 19:12:12 +0530

    nineth day include topic revise

--- Backend/eightDay_Instagram_EJS_Project/index.js
+++ Backend/eightDay_Instagram_EJS_Project/index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8080;
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 
 
